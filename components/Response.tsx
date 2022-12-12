e89094e228eb91dc4e0a6d9569da1d231008592a
efss7 <ericfelipess07@gmail.com>
2022-12-11 21:05:21 -0300

    Animação Resposta

--- components/Response.tsx
+++ components/Response.tsx
@@ -11,35 +11,36 @@ interface ResponseProps {
 
 export default function Response(props: ResponseProps) {
     const response = props.value
+    const answerRevealed = response.revealed ? styles.answerRevealed : ''
     return (
         <div className={styles.response}
             onClick={() => props.onResponse(props.index)}>
-            <div className={styles.contentResponse}>
-                {!response.revealed ? (
-                    <div className={styles.front}>
-                        <div className={styles.letter}
-                            style={{ backgroundColor: props.backgroundColorLetter }}>
-                            {props.letter}
-                        </div>
-                        <div className={styles.value} >
-                            {response.value}
-                        </div>
+            <div className={`${answerRevealed} ${styles.contentResponse}`}>
+
+                <div className={styles.front}>
+                    <div className={styles.letter}
+                        style={{ backgroundColor: props.backgroundColorLetter }}>
+                        {props.letter}
                     </div>
-                ) : (
-                    <div className={styles.verse}>
-                        {response.correct ? (
-                            <div className={styles.correct}>
-                                <div>A resposta correta é...</div>
-                                <div className={styles.value}>{response.value}</div>
-                            </div>
-                        ) : (
-                            <div className={styles.incorrect}>
-                                <div>A resposta informada está errada...</div>
-                                <div className={styles.value}>{response.value}</div>
-                            </div>
-                        )}
+                    <div className={styles.value} >
+                        {response.value}
                     </div>
-                )}
+                </div>
+
+                <div className={styles.verse}>
+                    {response.correct ? (
+                        <div className={styles.correct}>
+                            <div>A resposta correta é...</div>
+                            <div className={styles.value}>{response.value}</div>
+                        </div>
+                    ) : (
+                        <div className={styles.incorrect}>
+                            <div>A resposta informada está errada...</div>
+                            <div className={styles.value}>{response.value}</div>
+                        </div>
+                    )}
+                </div>
+
             </div>
         </div>
     )
